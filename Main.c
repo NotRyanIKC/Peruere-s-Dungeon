f748cf8ba867fb47fa8da9df423473fe0637a262
Koishi213 <168602824+Koishi213@users.noreply.github.com>
2024-05-15 15:39:53 -0300

    Update Main.c
    batatinha

--- Main.c
+++ Main.c
@@ -1,14 +1,15 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <stdbool.h> // manipular bool
-#include <string.h> // manipular string
-#include <conio.h> // getch()
+#include <stdbool.h>
+#include <string.h>
+#include <conio.h>
 
-char mapa[13][13];
+int level = 2;
+char mapa[23][23];
 
 typedef struct {
     bool vivo;
-    char nome[16]; // permite um nome de até 16 caracteres
+    char nome[16];
     int hp;
     int PlayerX;
     int PlayerY;
@@ -20,55 +21,62 @@ typedef struct {
 } player;
 
 int continuar = 1;
-player newPlayer; // declaro a existência do player
-/*Void loadmap(mapname){
-    If(mapname == 1){
-    Mapa = Loadmap(mapa1);
-    Printmap(mapa);
-    }
-}*/
-/*char layout2[23][23] = {
-		"\t\t\t********************",
-		"\t\t\t**	&	 *****#O#**",
-		"\t\t\t*		  ****	 **",
-		"\t\t\t*   		   ***  #**",
-		"\t\t\t*		 	**   **",
-		"\t\t\t*		 	 *#  **",
-		"\t\t\t*			      *",
-		"\t\t\t**			     **",
-		"\t\t\t*			      *",
-		"\t\t\t**			     **",
-		"\t\t\t***			    ***",
-		"\t\t\t***			    ***",
-		"\t\t\t***			    ***",
-		"\t\t\t**			     **",
-		"\t\t\t*			      *",
-		"\t\t\t**	   *	 X    *",
-		"\t\t\t** ##  ***	      *",
-		"\t\t\t**D## *****	 @   **",
-		"\t\t\t********************",
-		};*/
-void GenerateMap1(char mapa[13][13], int PlayerX, int PlayerY, int ChaveX, int ChaveY, int PortaX, int PortaY) {
+player newPlayer;
+
+void GenerateMap1(char mapa[23][23], int PlayerX, int PlayerY, int ChaveX, int ChaveY, int PortaX, int PortaY, int monsteX, int monsteY) {
     int i, j;
-    char layout[13][13] = {
-        "\t\t\t*********",
-        "\t\t\t**     **",
-        "\t\t\t*       *",
-        "\t\t\t*       *",
-        "\t\t\t*  ******",
-        "\t\t\t*  *@   *",
-        "\t\t\t*  ***  *",
-        "\t\t\t*  *D*  *",
-        "\t\t\t*       *",
-        "\t\t\t*********",
-    };
-    
-    for (i = 0; i < 13; i++) {
-        for (j = 0; j < 13; j++) {
-            mapa[i][j] = layout[i][j];
+
+    if (level == 1) {
+        char layout[13][13] = {
+            "\t\t\t*********",
+            "\t\t\t**     **",
+            "\t\t\t*       *",
+            "\t\t\t*       *",
+            "\t\t\t*  ******",
+            "\t\t\t*  *@   *",
+            "\t\t\t*  ***  *",
+            "\t\t\t*  *D*  *",
+            "\t\t\t*       *",
+            "\t\t\t*********",
+        };
+
+        for (i = 0; i < 13; i++) {
+            for (j = 0; j < 13; j++) {
+                mapa[i][j] = layout[i][j];
+            }
+        }
+    } else if (level == 2) {
+        char layout2[23][23] = {
+            "\t\t\t********************",
+            "\t\t\t**        *****#O#**",
+            "\t\t\t*          ****   **",
+            "\t\t\t*           ***  #**",
+            "\t\t\t*            **   **",
+            "\t\t\t*             *#  **",
+            "\t\t\t*                  *",
+            "\t\t\t**                **",
+            "\t\t\t*                  *",
+            "\t\t\t**                **",
+            "\t\t\t***              ***",
+            "\t\t\t***              ***",
+            "\t\t\t***              ***",
+            "\t\t\t**                **",
+            "\t\t\t*                  *",
+            "\t\t\t**      *          *",
+            "\t\t\t** ##  ***         *",
+            "\t\t\t**D## *****   @   **",
+            "\t\t\t********************",
+        };
+
+        for (i = 0; i < 23; i++) {
+            for (j = 0; j < 23; j++) {
+                mapa[i][j] = layout2[i][j];
+            }
         }
     }
+
     mapa[PlayerY][PlayerX] = '&';
+    mapa[monsteY][monsteX] = 'X';
     if (newPlayer.chavePega) {
         mapa[ChaveY][ChaveX] = ' ';
         mapa[PortaY][PortaX] = '=';
@@ -78,11 +86,10 @@ void GenerateMap1(char mapa[13][13], int PlayerX, int PlayerY, int ChaveX, int C
     }
 }
 
-
-void PrintMap(char mapa[13][13]) {
+void PrintMap(char mapa[23][23]) {
     int i, j;
-    for (i = 0; i < 13; i++) {
-        for (j = 0; j < 13; j++) {
+    for (i = 0; i < 23; i++) {
+        for (j = 0; j < 23; j++) {
             printf("%c", mapa[i][j]);
         }
         printf("\n");
@@ -103,11 +110,10 @@ void PrintTuto() {
     system("cls");
 }
 
-void PlayerSettingsBase() { // função só para facilitar o reset do game 
+void PlayerSettingsBase() {
     newPlayer.vivo = true;
     newPlayer.hp = 3;
     newPlayer.chavePega = false;
-
 }
 
 void sair() {
@@ -116,14 +122,10 @@ void sair() {
 }
 
 void Interact() {
-    // Verifica se o jogador está na posição da chave
     if (newPlayer.PlayerX == newPlayer.ChaveX && newPlayer.PlayerY == newPlayer.ChaveY && !newPlayer.chavePega) {
-        // Remove o '@' do mapa e marca como encontrado
         mapa[newPlayer.ChaveY][newPlayer.ChaveX] = ' ';
         newPlayer.chavePega = true;
         printf("Voce pegou a chave '@'!\n");
-
-        // Troca 'D' por '=' no mapa para indicar que a porta foi aberta
         mapa[newPlayer.PortaY][newPlayer.PortaX] = '=';
     } else if (newPlayer.PlayerX == newPlayer.PortaX && newPlayer.PlayerY == newPlayer.PortaY && newPlayer.chavePega) {
         printf("Voce abriu a porta e concluiu a fase!\n");
@@ -133,15 +135,55 @@ void Interact() {
     }
 }
 
-void GameStart() {
-    newPlayer.PlayerX = 7; // Coordenada X inicial do jogador
-    newPlayer.PlayerY = 1; // Coordenada Y inicial do jogador
-    newPlayer.ChaveX = 7;
-    newPlayer.ChaveY = 5;
-    newPlayer.PortaX = 7;
-    newPlayer.PortaY = 7;
+#include <stdlib.h> // Para rand()
 
-    GenerateMap1(mapa, newPlayer.PlayerX, newPlayer.PlayerY, newPlayer.ChaveX, newPlayer.ChaveY, newPlayer.PortaX, newPlayer.PortaY);
+void monstmov(int *monsteX, int *monsteY) {
+    int n = 4;
+
+    int movement = rand() % (n + 1);
+
+    if (movement == 1) {
+        (*monsteX)++;
+    } else if (movement == 2) {
+        (*monsteX)--;
+    } else if (movement == 3) {
+        (*monsteY)++;
+    } else {
+        (*monsteY)--;
+    }
+}
+
+void GameStart() {
+    int monsteX, monsteY;
+
+    if (level == 1) {
+        newPlayer.PlayerX = 7;
+        newPlayer.PlayerY = 1;
+
+        newPlayer.ChaveX = 7;
+        newPlayer.ChaveY = 5;
+
+        newPlayer.PortaX = 7;
+        newPlayer.PortaY = 7;
+
+        monsteX = 0;
+        monsteY = 0;
+
+    } else if (level == 2) {
+        newPlayer.PlayerX = 6;
+        newPlayer.PlayerY = 1;
+
+        newPlayer.ChaveX = 17;
+        newPlayer.ChaveY = 17;
+
+        newPlayer.PortaX = 5;
+        newPlayer.PortaY = 17;
+
+        monsteX = 18;
+        monsteY = 14;
+    }
+
+    GenerateMap1(mapa, newPlayer.PlayerX, newPlayer.PlayerY, newPlayer.ChaveX, newPlayer.ChaveY, newPlayer.PortaX, newPlayer.PortaY, monsteX, monsteY);
 
     char command;
 
@@ -150,33 +192,37 @@ void GameStart() {
         command = getch();
 
         switch (command) {
-            case 'W':
-            case 'w':
-                if (mapa[newPlayer.PlayerY - 1][newPlayer.PlayerX] != '*' && mapa[newPlayer.PlayerY - 1][newPlayer.PlayerX] != 'D') // Verifica se a próxima posição não é uma parede
-                    newPlayer.PlayerY--;
-                break;
-            case 'S':
-            case 's':
-                if (mapa[newPlayer.PlayerY + 1][newPlayer.PlayerX] != '*' && mapa[newPlayer.PlayerY + 1][newPlayer.PlayerX] != 'D') // Verifica se a próxima posição não é uma parede
-                    newPlayer.PlayerY++;
-                break;
-            case 'D':
-            case 'd':
-                if (mapa[newPlayer.PlayerY][newPlayer.PlayerX + 1] != '*' && mapa[newPlayer.PlayerY][newPlayer.PlayerX + 1] != 'D') // Verifica se a próxima posição não é uma parede
-                    newPlayer.PlayerX++;
-                break;
-            case 'A':
-            case 'a':
-                if (mapa[newPlayer.PlayerY][newPlayer.PlayerX - 1] != '*' && mapa[newPlayer.PlayerY][newPlayer.PlayerX - 1] != 'D') // Verifica se a próxima posição não é uma parede
-                    newPlayer.PlayerX--;
-                break;
-            case 'I':
-            case 'i':
-                Interact();
-                break;
+        case 'W':
+        case 'w':
+            if (mapa[newPlayer.PlayerY - 1][newPlayer.PlayerX] != '*' && mapa[newPlayer.PlayerY - 1][newPlayer.PlayerX] != 'D')
+                newPlayer.PlayerY--;
+            monstmov(&monsteX, &monsteY);
+            break;
+        case 'S':
+        case 's':
+            if (mapa[newPlayer.PlayerY + 1][newPlayer.PlayerX] != '*' && mapa[newPlayer.PlayerY + 1][newPlayer.PlayerX] != 'D')
+                newPlayer.PlayerY++;
+            monstmov(&monsteX, &monsteY);
+            break;
+        case 'D':
+        case 'd':
+            if (mapa[newPlayer.PlayerY][newPlayer.PlayerX + 1] != '*' && mapa[newPlayer.PlayerY][newPlayer.PlayerX + 1] != 'D')
+                newPlayer.PlayerX++;
+            monstmov(&monsteX, &monsteY);
+            break;
+        case 'A':
+        case 'a':
+            if (mapa[newPlayer.PlayerY][newPlayer.PlayerX - 1] != '*' && mapa[newPlayer.PlayerY][newPlayer.PlayerX - 1] != 'D')
+                newPlayer.PlayerX--;
+            monstmov(&monsteX, &monsteY);
+            break;
+        case 'I':
+        case 'i':
+            Interact();
+            break;
         }
         system("cls");
-        GenerateMap1(mapa, newPlayer.PlayerX, newPlayer.PlayerY, newPlayer.ChaveX, newPlayer.ChaveY, newPlayer.PortaX, newPlayer.PortaY);
+        GenerateMap1(mapa, newPlayer.PlayerX, newPlayer.PlayerY, newPlayer.ChaveX, newPlayer.ChaveY, newPlayer.PortaX, newPlayer.PortaY, monsteX, monsteY);
         PrintMap(mapa);
         if (mapa[newPlayer.PortaY][newPlayer.PortaX] == '=' && newPlayer.PlayerX == newPlayer.PortaX && newPlayer.PlayerY == newPlayer.PortaY) {
             system("cls");
@@ -201,31 +247,31 @@ int main() {
         system("cls");
 
         switch (option) {
-            case 1:
-                PlayerSettingsBase();
-                GameStart(); // Iniciar o Jogo
-                break;
-            case 2:
-                system("cls");
-                PrintTuto();
-                if (!tutorial) {
-                    tutorial = true;
-                }
-                printf("\n\n");
-                printf("\t\t\t88^^Yb 888888 88^^Yb 88   88 888888 88^^Yb 888888  .o. .dP'Y8   8888b. 88   88 88b 88  dP^^b8  888888  dP^Yb  88b 88\n");
-                printf("\t\t\t88__dP 88__   88__dP 88   88 88__   88__dP 88__   ,dP' `Ybo.'   8I  Yb 88   88 88Yb88  dP   `' 88__   dP   Yb 88Yb88\n");
-                printf("\t\t\t88^^^  88^^   88^^Yb Y8   8P 88^^   88^^Yb 88^^        o.`Y8b   8I  dY Y8   8P 88 Y88  Yb  ^88 88^^   Yb   dP 88 Y88\n");
-                printf("\t\t\t88     888888 88  Yb ^YbodP^ 888888 88  Yb 888888      8bodP^   8888Y' `YbodP' 88  Y8  YboodP  888888  YbodP  88  Y8\n");
-                printf("\n\n");
-                 break;
-            case 3:
-                sair();//Sair do Jogo
-                break;
-            default:
-                printf("Opção invalida.");
+        case 1:
+            PlayerSettingsBase();
+            GameStart();
+            break;
+        case 2:
+            system("cls");
+            PrintTuto();
+            if (!tutorial) {
+                tutorial = true;
+            }
+            printf("\n\n");
+            printf("\t\t\t88^^Yb 888888 88^^Yb 88   88 888888 88^^Yb 888888  .o. .dP'Y8   8888b. 88   88 88b 88  dP^^b8  888888  dP^Yb  88b 88\n");
+            printf("\t\t\t88__dP 88__   88__dP 88   88 88__   88__dP 88__   ,dP' `Ybo.'   8I  Yb 88   88 88Yb88  dP   `' 88__   dP   Yb 88Yb88\n");
+            printf("\t\t\t88^^^  88^^   88^^Yb Y8   8P 88^^   88^^Yb 88^^        o.`Y8b   8I  dY Y8   8P 88 Y88  Yb  ^88 88^^   Yb   dP 88 Y88\n");
+            printf("\t\t\t88     888888 88  Yb ^YbodP^ 888888 88  Yb 888888      8bodP^   8888Y' `YbodP' 88  Y8  YboodP  888888  YbodP  88  Y8\n");
+            printf("\n\n");
+            break;
+        case 3:
+            sair();
+            break;
+        default:
+            printf("Opção invalida.");
         }
-    }while(option != 3);
-    
+    } while (option != 3);
+
     getch();
     return 0;
 }
